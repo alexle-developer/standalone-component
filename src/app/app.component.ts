f3513a34f40c4100c0e9e59d2e0c7398d6142f79
alexle-aff <ale@americanfirstfinance.com>
2023-02-08 16:11:23 -0600

    step 2 - revoe AppComponent from app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
-// Adding "standalone: true" causes this error NG6008: Component AppComponent is standalone,
+// Step 1: Adding "standalone: true"
+//
+// it causes this error NG6008: Component AppComponent is standalone,
 // and cannot be declared in an NgModule.Did you mean to import it instead?
 
 
@@ -8,8 +11,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [AppRoutingModule]
 })
+
 export class AppComponent {
   title = 'standalone-component';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+// Step 2: remove AppComponent from the declarations and bootstrap
+// to eliminate error NG6008: Component AppComponent is standalone,
+// and cannot be declared in an NgModule.Did you mean to import it instead?
+//
+// Now we have different error after completing Step 2.
+// Error: src / app / app.component.html: 31: 1 - error NG8001: 'router-outlet' is not a known element:
+// 1. If 'router-outlet' is an Angular component, then verify that it is included in the '@Component.imports' of this component.
+// 2. If 'router-outlet' is a Web Component then add 'CUSTOM_ELEMENTS_SCHEMA' to the '@Component.schemas' of this component to suppress this message.
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
