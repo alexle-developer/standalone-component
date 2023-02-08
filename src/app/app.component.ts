4cf22e63a69e2c8949602e63f71ff34f00823f58
alexle-aff <ale@americanfirstfinance.com>
2023-02-08 15:59:00 -0600

    add standalone:true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+// Adding "standalone: true" causes this error NG6008: Component AppComponent is standalone,
+// and cannot be declared in an NgModule.Did you mean to import it instead?
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true
 })
 export class AppComponent {
   title = 'standalone-component';
